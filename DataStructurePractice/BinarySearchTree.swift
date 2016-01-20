dc56ff9a6fdfd1f434fe3ffd3eed6307f12cd2ca
Kevin Taniguchi <ktaniguchi@urbn.com>
2016-01-19 22:03:32 -0500

    add node function lowest value from node key

--- DataStructurePractice/BinarySearchTree.swift
+++ DataStructurePractice/BinarySearchTree.swift
@@ -13,19 +13,64 @@ public class BSTree<T: Comparable> {
     var left: BSTree?
     var right: BSTree?
     
+    // to make this bi-directional add
+    // var previousLeft: BSTree?
+    // var previousRight: BSTree?
+    
     init() {
     }
     
-    func addNode(key: T) {
+    func addNode(addedKey: T) {
+        if key == nil {
+            key = addedKey
+            return
+        }
         
+        if addedKey < key {
+            if left == nil {
+                let newLeftNode = BSTree()
+                newLeftNode.key = addedKey
+                left = newLeftNode
+            }
+            else {
+                guard let currentLeft = left else { return }
+                currentLeft.addNode(addedKey)
+            }
+        }
+        
+        if addedKey > self.key {
+            if right == nil {
+                let newRightNode = BSTree()
+                newRightNode.key = addedKey
+                right = newRightNode
+            }
+            else {
+                guard let currentRight = right else { return }
+                currentRight.addNode(addedKey)
+            }
+        }
     }
     
-    func lowestValueNode() {
+//    func findHeadFromKey(key: T) -> BSTree? {
+//        
+//    }
+    
+    func lowestValueNodeFrom(node: BSTree?) -> BSTree? {
+        if left == nil && right == nil {
+            return self
+        }
         
+        if let leftKey = left where leftKey.key < key {
+            return lowestValueNodeFrom(node)
+        }
+        if let rightKey = right where rightKey.key < key {
+            return lowestValueNodeFrom(node)
+        }
+        return nil
     }
     
-    func highestValueNode() {
-        
-    }
+//    func highestValueNode() -> BSTree? {
+//        
+//    }
 }
 
